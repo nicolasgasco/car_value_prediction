f831ebf0db7ce845b7eda8a44e19549a18e13ac2
Nicolas Gasco <nicolas.gasco92@gmail.com>
2024-03-30 00:50:32 +0100

    Load and transform data in DataHandler class

---
+++ app/DataHandler.py
@@ -0,0 +1,66 @@
+import json as json
+import os as os
+import pandas as pd
+import re as re
+
+DATA_DIRECTORY_PATH = "../data"
+
+
+class DataHandler:
+    def __init__(self):
+        self._raw_listings = []
+        self._listings = []
+
+    def load_data(self):
+        """
+        Loads data from JSON files in the specified directory and stores it in the `_raw_listings` attribute.
+
+        Returns:
+            None
+        """
+
+        raw_listings = []
+        for filename in os.listdir(DATA_DIRECTORY_PATH):
+            if filename.endswith(".json"):
+                file_path = os.path.join(DATA_DIRECTORY_PATH, filename)
+
+                with open(file_path, "r") as file:
+                    file_data = file.read()
+                    parsed_file_data = json.loads(file_data)
+
+                    file_raw_listings = parsed_file_data["pageProps"]["listings"]
+                    raw_listings.extend(file_raw_listings)
+
+        self._raw_listings = raw_listings
+
+    def _transform_listing(self, listing):
+        """
+        Transforms a car listing into a dictionary containing the price and mileage.
+
+        Args:
+            listing (dict): A dictionary representing a car listing.
+
+        Returns:
+            dict: A dictionary containing the price and mileage of the car listing.
+        """
+
+        raw_price = listing["price"]["priceFormatted"]
+        raw_price = raw_price.replace(".", "")
+        price = float(re.sub(r"[^\d.]", "", raw_price))
+
+        mileage = int(listing["tracking"]['mileage'])
+
+        return {"price": price, "mileage": mileage}
+
+    def create_listings_df(self):
+        """
+        Creates a DataFrame from the raw listings data.
+
+        Returns:
+            pandas.DataFrame: The DataFrame containing the formatted listings data.
+        """
+
+        formatted_listings = map(self._transform_listing, self._raw_listings)
+
+        df = pd.DataFrame(formatted_listings)
+        self._listings = df
--- app/main.py
+++ app/main.py
@@ -1,5 +1,11 @@
+from DataHandler import DataHandler
+
+
 def main():
-    pass
+    data_handler = DataHandler()
+    data_handler.load_data()
+    data_handler.create_listings_df()
+
 
 if __name__ == "__main__":
     main()
