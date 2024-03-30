33b3d743353f0de0a9f4ba8c2c551c8b79b9287f
Nicolas Gasco <nicolas.gasco92@gmail.com>
2024-03-30 11:23:15 +0100

    Add basic App content

--- interface/src/App.tsx
+++ interface/src/App.tsx
@@ -4,30 +4,22 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [userMileage, setUserMileage] = useState<number>(0)
+
+  const handleUserMileageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setUserMileage(Number(event.target.value))
+  }
 
   return (
     <>
+      <h1>Car price estimate</h1>
+      <p>How much is my 2019-2020 Open Corsa worth?</p>
+
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <label htmlFor="mileage">Mileage</label>
+        <input type="range" min="1000" max="200000" step="1000" value={userMileage} id="mileage" onChange={handleUserMileageChange} />
+        <p>{userMileage}</p>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
